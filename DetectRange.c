fcac94991dd2220803fcd52dc273f311845c4620
Padmashree-DJ <77479478+Padmashree-DJ@users.noreply.github.com>
2021-04-29 20:12:59 +0530

    Update DetectRange.c

--- DetectRange.c
+++ DetectRange.c
@@ -46,7 +46,7 @@ void SortArray(int *ChargingCurrentReadings, int numOfCCReadings)
 void DetectContinuousRange( int *ChargingCurrentReadings, int numOfCCReadings)
 {
 	int StartIndex=0;
-	_Bool IsStart=1;
+	bool IsStart=1;
 	int Iscontinuous=1;
 	SortArray(ChargingCurrentReadings, numOfCCReadings);
 	for (int i=0; i<numOfCCReadings; i++)
